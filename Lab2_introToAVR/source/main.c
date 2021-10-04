635ada741d96aa7482da2be8ef735234f621fd49
Sean Sponagle <sspon001@wch136-22.cs.ucr.edu>
2021-10-03 21:43:22 -0700

    exercise4

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -13,17 +13,32 @@
 #endif
 
 int main(void) {
-	DDRA = 0x00 ; PORTA = 0xFF ;
-	DDRB = 0xFF ; PORTB = 0x00 ;
-	unsigned char cntavail = 0x04 ;
+	DDRA = 0x00 ; PORTA = 0xFF ;	
+	DDRB = 0x00 ; PORTB = 0xFF ;
+	DDRC = 0x00 ; PORTC = 0xFF ;
+	DDRD = 0xFF ; PORTD = 0x00 ;
+	unsigned char temp = 0x00 ;
+	unsigned char temp2 = 0x00 ;
+	unsigned char temp3 = 0x00 ;
+	unsigned char a = 0x00 ;
+	unsigned char b = 0x00 ;
+	unsigned char c = 0x00 ;
+	unsigned short totalWeight = 0x0000 ;
     while (1) {
-        cntavail = 0x04 ;
-	if(PINA & 0x01) cntavail -= 0x01 ;
-	if(PINA & 0x02) cntavail -= 0x01 ;
-	if(PINA & 0x04) cntavail -= 0x01 ;
-	if(PINA & 0x08) cntavail -= 0x01 ;
-	PORTC = 0xF & cntavail ;
-	if(cntavail == 0x00) PORTC = 0x8F ;	
+	a = PINA ;
+	b = PINB ;
+	c = PINC ;
+	if(a > c){
+		if((a - c) > 0x50) temp2 = 0x02 ;
+	}
+	else{
+		if((c - a) > 0x50) temp2 = 0x02 ;
+	}
+	totalWeight = a + b + c ;
+	if(totalWeight > 0x008C) temp = 0x01 ;	
+	else temp = 0x00 ;
+	temp3 = (totalWeight & 0x00FC) | temp | temp2 ;
+	PORTD = temp3 ;	
     }
     return 0 ;
 }
